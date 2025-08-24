5b0080253b4a364405d671e765c3adbb1b221558
nafis-ikbal <nafis24iqbal@gmail.com>
2025-08-25 03:34:04 +0600

    added category page

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import "./Container.css";
-
+import Category from "./pages/category_page/Category";
 
 export default function Container() {
     return(
         <BrowserRouter>
             <div className="mainc">
-                {/* <Home/> */}
+                <Category/>
             </div>
         </BrowserRouter>
     );
---
+++ src/pages/category_page/Category.jsx
@@ -0,0 +1,14 @@
+import Card_Container from "../../components/card_container/Card_Container";
+import { useParams } from "react-router-dom";
+
+export default function Category() {
+    const { category } = useParams();
+
+    return(
+        <>
+            <Card_Container category={category}/>
+        </>
+    );
+}
+
+//this is used for 'card_container' component 
